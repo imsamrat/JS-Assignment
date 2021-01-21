37977d238fa089287b8d27583d8f99127928cf61
S.m.Samrat <imsamrat24@gmail.com>
2021-01-21 23:39:01 +0600

    Kilometer to Meter Convert

--- assignment.js
+++ assignment.js
@@ -1,1 +1,10 @@
 //https://github.com/s-m-samrat/assignment-3
+
+//Kilometer to Meter Convert
+function kilometerToMeter(kilometer){
+    var meter = kilometer * 1000; //we know that, 1 kilometer = 1000 meter
+    return meter;
+}
+
+var result = kilometerToMeter(5);
+console.log(result);
